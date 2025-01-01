381470af5d025c6305afac2d113bbd5f7aff58cf
jipconan <jipconan@gmail.com>
2025-01-01 23:09:22 +0800

    adjustment/update-v001

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
-from patreon.fetch_sessionid import fetch_session_id
 from patreon.fetch_all_post_ids import fetch_all_post_ids
 from patreon.fetch_impression import fetch_impression
-from google.create_google_sheet_add_data import create_google_sheet_add_data
+from google.add_data_to_google_sheet import add_data_to_google_sheet
+import os
+from dotenv import load_dotenv
+
+load_dotenv("patreon/.env")
 
 # Main lambda handler
 def lambda_handler(event, context):
@@ -9,8 +12,8 @@ def lambda_handler(event, context):
         # Fetch all post IDs
         post_ids = fetch_all_post_ids()
 
-        # Fetch session ID
-        session_id = fetch_session_id()
+        # Declare Patreon session ID
+        session_id = os.getenv("PATRON_SESSION_ID")
 
         # List to store post IDs and impressions
         impression_data = []
@@ -23,7 +26,7 @@ def lambda_handler(event, context):
 
         # Add the data to Google Sheets
         if impression_data:
-            sheet_id = create_google_sheet_add_data(impression_data)
+            sheet_id = add_data_to_google_sheet(impression_data)
             print(f"Data added to Google Sheet with ID: {sheet_id}")
 
     except Exception as e:
